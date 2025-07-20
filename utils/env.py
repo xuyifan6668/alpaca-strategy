452d8de57c92e51dd3074034c912c78f41deba77
xuyifan6668 <xuyifan6668@gmail.com>
2025-07-20 15:08:13 -0400

    Update API keys

--- utils/env.py
+++ utils/env.py
@@ -1,8 +1,8 @@
  ### API keys for Alpaca
  # Data keys
-DATA_KEY = "PK2JES4J482C9JD7JX0J"
-DATA_SECRET = "gU9ifuHCNZge9dEfozmNWrSFMEidZxzMEgYibLxE"
+DATA_KEY = ""
+DATA_SECRET = ""
 # Trade keys paper trading
-TRADE_KEY = "PK2JES4J482C9JD7JX0J"
-TRADE_SECRET = "gU9ifuHCNZge9dEfozmNWrSFMEidZxzMEgYibLxE"
+TRADE_KEY = ""
+TRADE_SECRET = ""
 DEBUG=True
